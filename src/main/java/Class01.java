06eccce1662bf12461dde456ebc03afa79c0866f
Sefa Eyer <eyersefa@gmail.com>
2023-02-24 00:03:54 +0300

    v5

--- src/main/java/Class01.java
+++ src/main/java/Class01.java
@@ -1,7 +1,9 @@
 public class Class01 {
+
     public static void main(String[] args) {
 
         System.out.println("1. calismam");
         System.out.println("v3");
+        System.out.println("Bugun hava yagisli");
     }
 }
